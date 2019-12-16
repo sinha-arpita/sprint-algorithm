1658c9f8fc98da17b72f09014094d7cb2cb7f405
Arpita Sinha <sinha.arpita@gmail.com>
2019-12-15 20:09:59 -0800

    MVP and Stretch

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,15 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
+    i=word.find("th")
+    if i==-1:   #first occurrence of th in the word is found
+        return 0
+    # We have found first occurance of "th" starting at index i. So we have 1
+    # So now call this function again with rest of word starting from i + 2 (length of "th")
     # TBC
     
-    pass
+    return 1+ count_th(word[i+2:])
+print(count_th("abcdethefthghitheth"))
+
+
+
